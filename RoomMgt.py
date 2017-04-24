619a87d43e2ea0baedfa484f7d94a3d7517ffeb2
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-24 23:38:41 +0300

    dojo class that constructs a doj with a list of all rooms

---
+++ RoomMgt.py
@@ -0,0 +1,12 @@
+ """
+  name : room management system.
+  authr : benjamin wacha
+  email: bmwachajr@gmail,com
+  descrption: a system to randomly allocate rooms to andela staff and fellows.
+
+"""
+def Dojo(object):
+  """This is the Dojo class, and andela facility"""
+  def __init__(self):
+  self.all_rooms = []
+  
