ecda8fb2903bb2a104ba0a36a3a3db66305762f2
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-24 23:02:47 +0300

    implemented tescase case _creates_room_successfully

--- testcases.py
+++ testcases.py
@@ -5,3 +5,19 @@
   descrption: a system to randomly allocate rooms to andela staff and fellows.
 
 """
+import unittest
+from unittest import TestCase
+from RoomMgt import Dojo
+
+class TestCreateRoom(unittest.TestCase):
+  def test_create_room_successfully(self):
+    self.Dojo = Dojo()
+    initial_room_count = len(Dojo.all_rooms)
+    blue_office = Dojo.create_room("Blue", "office")
+    self.assertTrue(blue_office)
+    new_room_count = len(Dojo.all_rooms)
+    self.assertEqual(new_room_count - initial_room_count, 1)
+
+
+if __name__ == "__main__":
+  unittest.main()
