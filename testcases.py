9248a8577d173c717d75fc44d471e322a65cefc9
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-24 23:05:42 +0300

    implemented tescase case to makes sure room name and type are given

--- testcases.py
+++ testcases.py
@@ -18,6 +18,13 @@ class TestCreateRoom(unittest.TestCase):
     new_room_count = len(Dojo.all_rooms)
     self.assertEqual(new_room_count - initial_room_count, 1)
 
-
+  def test_create_room_name_not_given(self):
+    with self.assertRaises(RuntimeError) as context:
+      empty_office = Dojo.create_room("","")
+      self.assertEqual(
+          'Couldnt create Room.',
+          context.exception.message,
+          'Both Room Name and Room Type must be given'
+      )
 if __name__ == "__main__":
   unittest.main()
