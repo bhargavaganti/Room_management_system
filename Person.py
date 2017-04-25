56faa00a2f94b0151655152582f0a02d2af6c228
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-25 11:11:45 +0300

    added classes for Person, Staff, Fellow in new model

---
+++ Person.py
@@ -0,0 +1,49 @@
+"""
+  name : room management system.
+  authr : benjamin wacha
+  email: bmwachajr@gmail,com
+  descrption: a system to randomly allocate rooms to andela staff and fellows.
+
+"""
+
+class Person(object):
+  def __init__(self):
+    self.name = 
+    
+  def add_person(self, person_type, person_name):
+    if person_type == "Staff":
+      self.person_name = person_name
+      person_name = Staff()
+
+class Staff(Person):
+  """
+    Staff member at andela.
+    
+    atrributes:
+      office_space
+      
+  """
+  def __init__(self):
+    self.office = ""
+
+        
+
+class Fellow(Person):
+  """
+    Fellow at andela.
+    
+    atrributes:
+      office_space
+      wants_accomodation
+      living_space
+      
+  """
+  def __init__(self):
+    self.office_space = ""
+    self.wants_accomodation = ""
+    if wants_accomodation == "Y"
+      self.living_space = ""
+    else:
+      self.living_space = ""
+    
+ 
