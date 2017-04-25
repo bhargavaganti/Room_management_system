a378e4738c55fb8cadc9388aa2ba639cbd7b7126
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-25 10:45:54 +0300

    fixed Djo instcnce indetifiers

--- testcases.py
+++ testcases.py
@@ -11,29 +11,31 @@ from RoomMgt import Dojo
 
 class TestCreateRoom(unittest.TestCase):
   def test_create_room_successfully(self):
-    self.Dojo = Dojo()
-    initial_room_count = len(Dojo.all_rooms)
-    blue_office = Dojo.create_room("Blue", "office")
+    dojo = Dojo()
+    initial_room_count = len(dojo.all_rooms)
+    blue_office = dojo.create_room("Blue", "office")
     self.assertTrue(blue_office)
-    new_room_count = len(Dojo.all_rooms)
+    new_room_count = len(dojo.all_rooms)
     self.assertEqual(new_room_count - initial_room_count, 1)
 
   def test_create_room_name_not_given(self):
+    dojo = Dojo()
     with self.assertRaises(RuntimeError) as context:
-      empty_office = Dojo.create_room("","")
+      empty_office = dojo.create_room("","")
       self.assertEqual(
           'Couldnt create Room.',
           context.exception.message,
           'Both Room Name and Room Type must be given'
       )
       
-    def test_create_room_type_not_given(self):
-      with self.assertRaises(RuntimeError) as context:
-        no_type_office = Dojo.create_room("no_type", "")
-        self.assertEqual(
-            'Couldnt create Room.',
-            context.exception.message,
-            'Both Room Name and Room Type must be given'
-        )
+  def test_create_room_type_not_given(self):
+    dojo = Dojo()
+    with self.assertRaises(RuntimeError) as context:
+      no_type_office = dojo.create_room("no_type", "")
+      self.assertEqual(
+          'Couldnt create Room.',
+          context.exception.message,
+          'Both Room Name and Room Type must be given'
+      )
 if __name__ == "__main__":
   unittest.main()
