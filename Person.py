e3f41f0949147be231c7de828cb21a9849a568b9
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-25 13:31:17 +0300

    modifies fellow and staff constructors

--- Person.py
+++ Person.py
@@ -3,36 +3,42 @@
   authr : benjamin wacha
   email: bmwachajr@gmail,com
   descrption: a system to randomly allocate rooms to andela staff and fellows.
-
 """
 
+import sys
+
 class Person(object):
-  def __init__(self):
-    self.person_name = ''
+  def __init__(self, person_name):
+    self.person_name = person_name
     
-  def add_person(self, person_type, person_name):
+  def add_person(self, *args):
+    person_type = args[0]
+    self.person_name = args[1]
+    
     #if the person is a staff, create staff instance
-    if person_type == "Staff":
-      self.person_name = person_name
+    if len(args) == 2:
       person_name = Staff()
       return self
       
-    #if the person is a staff, create Fellow instance
-    elif person_type == "Fellow":
-      self.person_name = person_name
-      person_name = fellow()
+    #if the person is a fellow, create Fellow instance
+    elif len(args) == 3:
+      wants_accomodation = args[2]
+      self.wants_accomodation = wants_accomodation
+      print(self.wants_accomodation)
       return self
 
 class Staff(Person):
   """
     Staff member at andela.
     
-    atrributes:
+    Atrributes:
       office_space
       
   """
+  
+  #assign staff an office space
   def __init__(self):
-    self.office = ""
+    self.office_space = ""
 
         
 
@@ -40,16 +46,17 @@ class Fellow(Person):
   """
     Fellow at andela.
     
-    atrributes:
+    Atrributes:
       office_space
       wants_accomodation
       living_space
       
   """
   def __init__(self):
+    #self.wants_accomodation = wants_accomodation
     self.office_space = ""
-    self.wants_accomodation = ""
-    if wants_accomodation == "Y":
+    self.wants_accomodation = 
+    if self.wants_accomodation == "Y":
       self.living_space = ""
     else:
       self.living_space = ""
--- testcases.py
+++ testcases.py
@@ -8,7 +8,7 @@
 import unittest
 from unittest import TestCase
 from RoomMgt import Dojo
-from Person import Person
+from Person import Person, Staff, Fellow
 
 class TestCreateRoom(unittest.TestCase):
   def test_create_room_successfully(self):
@@ -40,15 +40,16 @@ class TestCreateRoom(unittest.TestCase):
       )
       
   def test_adds_Staff_successfully(self):
-    Staff = Person()
-    Staff_Henry = Staff.add_person("Staff", "Henry")
+    staff = Staff()
+    Staff_Henry = staff.add_person("staff", "Henry")
     self.assertTrue(Staff_Henry)
     self.assertEqual(Staff_Henry.person_name, "Henry", msg='Name should be Henry')
-    
+    self.assertEqual(Staff_Henry.office_space, "", msg='Name should be Henry')
+
   def test_adds_Fellow_successfully(self):
-    self.Fellow = Person()
-    fellow_Albert = Fellow.add_person("Fellow", "Albert", "Y")
-    self.assertEqual(self.fellow_Albert.name, "Henry", msg='Name should be Henry')
+    fellow = Fellow()
+    fellow_Albert = fellow.add_person("Fellow", "Albert", "Y")
+    self.assertEqual(fellow_Albert.person_name, "Albert", msg='Name should be Henry')
     self.assertEqual(fellow_Albert.wants_accomodation, "Y", msg='Name should be Albert')
     
 if __name__ == "__main__":
