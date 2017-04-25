098a54f6f5f2663df491c1b221d9cae3f5cac721
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-25 18:47:06 +0300

    A massive fix of bugs in code

--- Person.py
+++ Person.py
@@ -1,17 +1,20 @@
 """
-  name : room management system.
-  authr : benjamin wacha
-  email: bmwachajr@gmail,com
-  descrption: a system to randomly allocate rooms to andela staff and fellows.
+  Name : room management system.
+  Authr : benjamin wacha
+  Email: bmwachajr@gmail,com
+  Descrption: a system to randomly allocate rooms to andela staff and fellows.
 """
 
 import sys
+from RoomMgt import Dojo, Room
 
 class Person(object):
+  """ person at the Dojo"""
   def __init__(self, person_name):
     self.person_name = person_name
     
   def add_person(self, *args):
+    """Adding a new person at the dojo"""
     person_type = args[0]
     self.person_name = args[1]
     
@@ -26,38 +29,32 @@ class Person(object):
       self.wants_accomodation = wants_accomodation
       print(self.wants_accomodation)
       return self
+      
+    def allocate_office(self, office):
+      self.office = office;
+      
+    def allocate_livingSpace(self, livingSpace):
+      self.office = livingSpace;
 
 class Staff(Person):
   """
-    Staff member at andela.
-    
+    Staff member at andela.     
     Atrributes:
-      office_space
-      
+    office_space
   """
-  
-  #assign staff an office space
-  def __init__(self):
-    self.office_space = ""
+  pass
 
         
 
 class Fellow(Person):
+  """ Fellow at andela. Atrributes: office_space, wants_accomodation,   living_space
   """
-    Fellow at andela.
+  def __init__(self, wants_accomodation):
+    self.wabts_accomodation = wants_accomodation
     
-    Atrributes:
-      office_space
-      wants_accomodation
-      living_space
-      
-  """
-  def __init__(self):
     #self.wants_accomodation = wants_accomodation
-    self.office_space = ""
-    self.wants_accomodation = ""
     if self.wants_accomodation == "Y":
-      self.living_space = True
+      self.living_space = random_LivingSpace()
     else:
       self.living_space = False
     
--- RoomMgt.py
+++ RoomMgt.py
@@ -9,18 +9,35 @@ import sys
 
 class Dojo(object):
   """This is the Dojo class, and andela facility"""
+  
+  #initializa an empty list of all rooms
   def __init__(self):
+    self.all_livingSpace = []
+    self.all_offices = []
     self.all_rooms = []
   
+  
   def create_room(self, *args):
-    #if a single room is created 
+    """This method creates a room at the Dojo
+      
+    Attributes: room_name, room_type
+    """
+    #To create only a single room at the Dojo 
     if len(args) == 2:
       room_name = args[0]
       room_type = args[1]
-      if (room_name == "") or (room_type == ""):
-        raise RuntimeError("Couldnt create Room, both Room Name and Room Type needed")      
-        return room_object
       
+      #Donot create rooms at the dojo is name or type is empty
+      if (room_name == "") or (room_type == ""):
+        raise RuntimeError("Couldnt create Room, both Room Name and Room Type needed")
+      
+      elif(room_object == "office"):
+        room_object = room_name + "_" + room_type
+        room_object = Room(room_name, room_type)
+        self.all_offices.append(room_object)
+        print ("An " + room_type + " called " + room_name + " has been created successfully \n" )
+        
+        return room_object
       else:
         room_object = room_name + "_" + room_type
         room_object = Room(room_name, room_type)
@@ -31,32 +48,40 @@ class Dojo(object):
     
   
       
-    #is multiple rooms are created    
+    #To create multiple rooms of the same type at the Dojo   
     if len(args) > 2:
-      count = 0
+
       room_type = args[len(args)-1]
-      while count < (len(args)-1):
-        room_name = args[count]
+      room_group = args[0:len(args)-1]
+      
+      for room_name in room_group:
         room_object = room_name + "_" + room_type
         room_object = Room(room_name, room_type)
         self.all_rooms.append(room_object)
-        count += 1
         
         print ("An " + room_type + " called " + room_name + " has been created successfully \n" )
+        
       return room_object
+      
+    def get_random_office(self):
+      office = random.choice(self.all_offices)
+      return office
+      
+    def get_random_livingSpace(self):
+      livingSpace = random.choice(self.all_livingSpace)
+      return livingSpace
       
   
 class Room(object):
   """
-      Rooms at The Dojo
-      
-      attributes:
-        Room name
-        Room Occupants
+  Rooms at The Dojo
+  Attributes:
+  Room name
+  Room Occupants
   """
   def __init__(self, room_name, room_type):
     self.room_name = room_name
-    
+  
 class livingSpace(Room):
   max_people = 4
     
